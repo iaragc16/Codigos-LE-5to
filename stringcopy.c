9f23d0365a74e4def996cb08feab0f17d0b9fc10
iaragc16 <igarcia@etrr.edu.ar>
2023-11-23 15:52:16 -0300

    .

--- stringcopy.c
+++ stringcopy.c
@@ -5,10 +5,10 @@
 int main (void){
     char txt1[]="Hola"; //El primer string dice hola
     char txt2[CANT_MAX]; //El segundo string tiene 5 espacios
-    int i=0; 
+    //int i=0; 
     strcpy(txt2,txt1); //Al string 2 le copio el valor del string 1
-    while(txt2[i])printf("%c",txt2[i++]); /*mientras que el index (i) valga caracteres positivos del string, 
-    se van a imprimir los valores de los caracteres del string empezando en 0 y aumentando de a uno hasta el 5.
-    Es decir que se imprimiran todos los caracteres del string 2 hasta llegar al nule '0' o caracter final */
+    printf("%s",txt2);
+    //while(txt2[i])printf("%c",txt2[i++]); (otra forma de imprimir el string)
+    //se van a imprimir todos los caracteres del string desde el caracter i hasta el nule
     return 0;
 }
